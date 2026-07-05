0876ee86f20742f7ff79571efc4c839dc2bac169
hassanaym83 <essadikhassan83@gmail.com>
2026-07-05 18:55:54 +0100

    refactor: make OrderController call directly the orderService containing the busness logic

--- api/src/main/java/com/nimbleways/springboilerplate/contollers/OrderController.java
+++ api/src/main/java/com/nimbleways/springboilerplate/contollers/OrderController.java
@@ -1,20 +1,9 @@
 package com.nimbleways.springboilerplate.contollers;
 
 import com.nimbleways.springboilerplate.dto.product.ProcessOrderResponse;
-import com.nimbleways.springboilerplate.entities.Order;
-import com.nimbleways.springboilerplate.entities.Product;
-import com.nimbleways.springboilerplate.repositories.OrderRepository;
-import com.nimbleways.springboilerplate.repositories.ProductRepository;
 import com.nimbleways.springboilerplate.services.api.OrderService;
-import com.nimbleways.springboilerplate.services.implementations.ProductServiceImpl;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
-public class MyController {
+public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("{orderId}/processOrder")
