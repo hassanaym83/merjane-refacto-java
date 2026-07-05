7864297f40866ed391259e738f472d24d392d65d
hassanaym83 <essadikhassan83@gmail.com>
2026-07-05 18:41:48 +0100

    test: write the failed seasonal product tests

--- api/src/test/java/com/nimbleways/springboilerplate/services/implementations/ProductServiceImplTest.java
+++ api/src/test/java/com/nimbleways/springboilerplate/services/implementations/ProductServiceImplTest.java
@@ -12,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.not;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -108,6 +110,72 @@ class ProductServiceImplTest {
         void setUp() {
             product.setType(ProductType.SEASONAL);
             product.setName("Seasonal Product");
+        }
+
+        @Test
+        void inSeasonAndAvailableGtZero_decrementsAvailableAndSaveProduct(){
+            //Arrange
+            product.setSeasonStartDate(LocalDate.now().minusDays(1));
+            product.setSeasonEndDate(LocalDate.now().plusDays(1));
+            product.setAvailable(10);
+
+            //Act
+            productService.handleSeasonalProduct(product);
+
+            //Assert
+            assertThat(product.getAvailable()).isEqualTo(9);
+            verify(productRepository).save(product);
+        }
+
+        @Test
+        void seasonEndsBeforeLeadTime_setAvailableToZeroAndSendOutOfStockNotificationAndSaveProduct(){
+            //Arrange
+            product.setAvailable(10);
+            product.setLeadTime(10);
+            product.setSeasonStartDate(LocalDate.now().minusDays(2));
+            product.setSeasonEndDate(LocalDate.now().minusDays(9));
+
+            //Act
+            productService.handleSeasonalProduct(product);
+
+            //Assert
+            assertThat(product.getAvailable()).isEqualTo(0);
+            verify(notificationService).sendOutOfStockNotification(product.getName());
+            verify(productRepository).save(product);
+        }
+
+        @Test
+        void seasonInTheFuture_saveProductAndSendOutOfStockNotification(){
+            //Arrange
+            product.setAvailable(10);
+            product.setLeadTime(10);
+            product.setSeasonStartDate(LocalDate.now().plusDays(1));
+            product.setSeasonEndDate(LocalDate.now().plusDays(10));
+
+            //Act
+            productService.handleSeasonalProduct(product);
+
+            //Assert
+            assertThat(product.getAvailable()).isEqualTo(10);
+            verify(notificationService).sendOutOfStockNotification(product.getName());
+            verify(productRepository).save(product);
+
+        }
+
+        @Test
+        void inSeasonAndAvailableEqZeroAndLeadTimeBeforeSeasonEnds_sendDelayNotification(){
+            //Arrange
+            product.setAvailable(0);
+            product.setLeadTime(10);
+            product.setSeasonStartDate(LocalDate.now().minusDays(1));
+            product.setSeasonEndDate(LocalDate.now().plusDays(20));
+
+            //Act
+            productService.handleSeasonalProduct(product);
+
+            //Assert
+            verify(notificationService).sendDelayNotification(product.getLeadTime(), product.getName());
+            verify(productRepository, never()).save(product);
         }
     }
 
